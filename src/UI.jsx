1a33dbe533c3af784b1f108069004f696a94c8d5
Super Zombi <super.zombi.yt@gmail.com>
2026-07-28 01:21:28 +0100

    Update UI.jsx

--- src/UI.jsx
+++ src/UI.jsx
@@ -219,7 +219,7 @@ const TextInput = ({ value, onChange, label, onKeyDown, placeholder, colorPicker
 				<input type="text" value={value} placeholder={placeholder}
 					onInput={e => onChange(e.target.value)}
 					onKeyDown={onKeyDown}
-					className="flex-1 bg-white/5 ring ring-white/10 rounded-lg px-3 py-2 outline-none
+					className="flex-1 bg-white/5 min-w-0 ring ring-white/10 rounded-lg px-3 py-2 outline-none
 						focus:ring-white/30 transition-colors placeholder:text-white/30"
 				/>
 			</div>
