d04b33f2b4fb961eac72c84d27f963f31f087333
Super Zombi <super.zombi.yt@gmail.com>
2026-07-31 19:10:09 +0100

    fix button style

--- src/UI.jsx
+++ src/UI.jsx
@@ -9,7 +9,7 @@ const Container = ({children, className="", href, target="_blank", ...props}) =>
 	)
 }
 
-const Button = ({className="", accent, children, forceActive, onClick, href, target="_blank", ...props}) => {
+const Button = ({className="", accent, children, forceActive, onClick, href, target="_blank", style, ...props}) => {
 	return (
 		<Container className={`
 			p-2 flex items-center justify-center gap-2 select-none whitespace-nowrap cursor-pointer transition-colors
@@ -20,7 +20,10 @@ const Button = ({className="", accent, children, forceActive, onClick, href, tar
 		`}
 			href={href} target={target}
 			onClick={onClick}
-			style={accentStyle(accent, { bg: forceActive ? 25 : 10, ring: 40, hoverBg: 25 })}
+			style={{
+				...accentStyle(accent, { bg: forceActive ? 25 : 10, ring: 40, hoverBg: 25 }),
+				...style,
+			}}
 			{...props}
 		>
 			{children}
